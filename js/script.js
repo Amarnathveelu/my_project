f222c442ce48d7bd9649cdc53ecdeb7db5ea18a2
Amarnathveelu <amarnathveelu@gmail.com>
2026-02-19 00:16:49 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var Object_data = {
+    key1:"valiue1",
+    key2:"valiue2",
+    key3:"valiue3",
+    key4:"valiue4",
+}
+
+
+console.log(Object_data.key3);
+
+
+var std_datas = {
+    company:"zhahi tech" ,
+}
